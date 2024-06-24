0c9e05533cf9a2c7a5afd7184b621e1b2d24534f
Soumya ! <soumyadipadak99@gmail.com>
2024-06-24 17:33:40 +0530

    WAP to count the numbers of students wiht the 'A' grade in the following tuple.

---
+++ LIst $ Tuple/Tuple/practice_Question_1.py
@@ -0,0 +1,4 @@
+# WAP to count the numbers of students wiht the "A" grade in the following tuple.
+
+tup = ("C", "C", "A", "B", "A")
+print("Total numbers of grade A present in tuple is:", tup.count("A"), "time")
