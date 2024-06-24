2baa50a710094fc8edc7e682cd72cb5e228c6744
Soumya ! <soumyadipadak99@gmail.com>
2024-06-24 17:02:46 +0530

    Input numbers from the user and set in a list and print the list

---
+++ LIst $ Tuple/Practice_question_1.py
@@ -0,0 +1,9 @@
+# input numbers from the user and set in a list and print the list
+
+number = []
+
+number.append(int(input("Enter first number: ")))
+number.append(int(input("Enter second number: ")))
+number.append(int(input("Enter third number: ")))
+
+print("Enter Numbers:", number)
