24f274bc66e3794e45a7d14ab8685552bc31506d
Soumya ! <soumyadipadak99@gmail.com>
2024-06-24 12:03:53 +0530

    update

---
+++ LIst $ Tuple/List/List Methods/Insert_method.py
@@ -0,0 +1,5 @@
+# insert element at index
+
+list = [1, 2, 3, 4]
+list.insert(2, 5)
+print(list)
---
+++ LIst $ Tuple/List/List Methods/list_remove_method.py
@@ -0,0 +1,5 @@
+# removes first occurrence of element
+list = [1, 2, 3, 2, 4]
+list.remove(2)
+
+print(list)
