cb8775ceb3df974c657358895491ecfb269c8c3f
Soumya ! <soumyadipadak99@gmail.com>
2024-06-24 17:19:34 +0530

    Find palindrom in list

--- LIst $ Tuple/practice_question_2.py
+++ LIst $ Tuple/practice_question_2.py
@@ -1,6 +1,7 @@
 list = [1, 2, 3, 2, 1]  # Given list
 
 copy_list = list.copy()  # Create a copy of the list
+
 list.reverse()  # Reverse the original list
 
 # Check if the reversed list is equal to the copy
