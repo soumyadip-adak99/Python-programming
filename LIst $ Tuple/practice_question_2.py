01e7a2847c620a2eef8f6756db70e22b245a965c
Soumya ! <soumyadipadak99@gmail.com>
2024-06-24 17:16:44 +0530

    Find palindrom in list

---
+++ LIst $ Tuple/practice_question_2.py
@@ -0,0 +1,10 @@
+list = [1, 2, 3, 2, 1]  # Given list
+
+copy_list = list.copy()  # Create a copy of the list
+list.reverse()  # Reverse the original list
+
+# Check if the reversed list is equal to the copy
+if copy_list == list:
+    print("Palindrom")  # It's a palindrome!
+else:
+    print("NOT palindrom")  # It's not a palindrome.
