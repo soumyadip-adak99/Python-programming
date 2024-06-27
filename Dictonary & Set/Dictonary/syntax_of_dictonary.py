2f9d252fad62f75f5dbf29137306aa3872a5d94b
Soumya ! <soumyadipadak99@gmail.com>
2024-06-27 10:24:20 +0530

    Dictonary syntax

---
+++ Dictonary & Set/Dictonary/syntax_of_dictonary.py
@@ -0,0 +1,12 @@
+info = {
+    "key": "value",
+    "name": "Soumya",
+    "learning": "coding",
+    "age": 35,
+    "adult": True,
+    "marks": 89.5,
+    "subjetc": ["java", "python", "c++", "c", "java Script"],
+    "topic": ("dictonary", "set"),
+}
+
+print(info)
