32f47a837792dbb1bc3233990ba46898a07e76b5
Soumya ! <soumyadipadak99@gmail.com>
2024-07-02 16:04:08 +0530

    Store following word meanings in a python dictionary

---
+++ Dictonary & Set/practice_question1.py
@@ -0,0 +1,10 @@
+# Store following word meanings in a python dictionary:
+# table: "a piece of furniture", "List of facts & figures"
+# cat : "a small animal"
+
+dictionary = {
+    "cat": "a small animal",
+    "table": ["a pice of furniture", "List of facts & figures"],
+}
+
+print(dictionary)
