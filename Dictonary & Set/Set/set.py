d001971c16da5aa82718219efa8e06dd4503d1ee
Soumya ! <soumyadipadak99@gmail.com>
2024-07-02 12:10:36 +0530

    set syntax

---
+++ Dictonary & Set/Set/set.py
@@ -0,0 +1,9 @@
+collection = {1, 2, 3, "hello", "world"}
+# output
+print("Collection:", collection)
+print("Type of set:", type(collection))
+print("Length of set:", len(collection))
+
+# empty set
+empty_set = set()
+print("Empty set:", type(empty_set))
