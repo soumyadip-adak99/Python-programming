4462d353f8a7acf42851728feeb3cac0f60287bd
Soumya ! <soumyadipadak99@gmail.com>
2024-07-06 08:34:31 +0530

    update

---
+++ Loops/pattern_10.py
@@ -0,0 +1,14 @@
+#      *****
+#     *****
+#    *****
+#   *****
+#  *****
+
+n = 5
+
+for i in range(1, n + 1):
+    for j in range(0, n - i):
+        print(" ", end="")
+    for k in range(1, n + 1):
+        print("*", end="")
+    print()
