1004558b0c34625418cbb0645290194c35219dc6
Soumya ! <soumyadipadak99@gmail.com>
2024-08-06 20:34:16 +0530

    list user input

---
+++ LIst $ Tuple/List/User inputs/listInput.py
@@ -0,0 +1,10 @@
+size = int(input("Enter the size of the arrya: "))
+data = []
+
+print("Enter elements: ", end=" ")
+
+for i in range(size):
+    element = input()
+    data.append(element)
+
+print(data)
