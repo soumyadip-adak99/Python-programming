2cf28a4137b052082b730338b8f9b5aa4117958e
Soumya ! <soumyadipadak99@gmail.com>
2024-08-12 20:15:46 +0530

    Merge sort

--- LIst $ Tuple/List/Sort/Merge sort/merge_sort.py
+++ LIst $ Tuple/List/Sort/Merge sort/merge_sort.py
@@ -1,40 +1,42 @@
-def conqcure(left, right):
-    new_arr = []
+def concqure(arr, str_idx, mid, end_idx):
+    newArr = []
 
-    i = j = 0
+    idx_1 = str_idx
+    idx_2 = mid + 1
 
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            new_arr.append(left[i])
-            i += 1
+    while (idx_1 <= mid and idx_2 <= end_idx):
+        if arr[idx_1] < arr[idx_2]:
+            newArr.append(arr[idx_1])
+            idx_1 += 1
         else:
-            new_arr.append(right[j])
-            j += 1
+            newArr.append(arr[idx_2])
+            idx_2 += 1
 
-    while (i < len(left)):
-        new_arr.append(left[i])
-        i += 1
+    while (idx_1 <= mid):
+        newArr.append(arr[idx_1])
+        idx_1 += 1
 
-    while (j < len(right)):
-        new_arr.append(right[j])
-        j += 1
+    while (idx_2 <= end_idx):
+        newArr.append(arr[idx_2])
+        idx_2 += 1
 
-    return new_arr
+    for i in range(len(newArr)):
+        arr[str_idx + i] = newArr[i]
 
 
-def divided(arr):
-    if len(arr) == 1:
-        return arr
+def divide(arr, str_idx, end_idx):
+    if str_idx >= end_idx:
+        return
 
-    mid = len(arr) // 2
+    mid = str_idx + (end_idx - str_idx) // 2
 
-    left = divided(arr[:mid])
-    right = divided(arr[mid:])
-
-    return conqcure(left, right)
+    divide(arr, str_idx, mid)
+    divide(arr, mid + 1, end_idx)
+    concqure(arr, str_idx, mid, end_idx)
 
 
 if __name__ == "__main__":
-    arr = [5, 3, 4, 1, 2]
-    ans = divided(arr)
-    print(ans)
+    arr = [4, 6, 5, 1, 2, 3]
+    size = len(arr)
+    divide(arr, 0, size - 1)
+    print(arr)
