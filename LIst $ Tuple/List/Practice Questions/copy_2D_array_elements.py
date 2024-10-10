62ec8cb20e7830cf6225e3b3eee60e65865ee469
Soumya ! <soumyadipadak99@gmail.com>
2024-10-10 06:43:37 +0530

    Copy array elements

--- LIst $ Tuple/List/Practice Questions/copy_2D_array_elements.py
+++ LIst $ Tuple/List/Practice Questions/copy_2D_array_elements.py
@@ -1,19 +1,19 @@
 def copy_arr_elements(a, b):
-    print("Original array:",a) #print original arrya
-
-    #copy arr
+    print("A:",a)
+    
     for i in range(len(a)):
         for j in range(len(a[i])):
             b[i][j] = a[i][j]
-
-    print("Copy array:",b) #print copy array
+    
+    print("B:",b)
 
 if __name__ == "__main__":
     a = [
-        [1, 2, 3, 4],
-        [4, 5, 6, 7],
-        [8, 9, 0, 1]
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
     ]
+    
     b = [[0 for _ in range(len(a[0]))] for _ in range(len(a))]
-
+    
     copy_arr_elements(a, b)
