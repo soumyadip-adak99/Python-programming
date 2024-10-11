d0aee45fcdba266c5f79eb9aa015865d8fd87bf3
Soumya ! <soumyadipadak99@gmail.com>
2024-10-11 12:34:26 +0530

    Linklist implimentation

--- DSA/LinkedList/implement_singly_linkelist.py
+++ DSA/LinkedList/implement_singly_linkelist.py
@@ -3,67 +3,106 @@ class Node:
         self.data = data
         self.next = None
 
-class LL:
+class Linklist:
     def __init__(self):
         self.head = None
-        
-    def add_items_beginning(self, data):
+        self.size = 0
+
+    def addFirst(self, data): #add first
         new_node = Node(data)
-        
         if self.head is None:
             self.head = new_node
-            return  
-        
+            self.size += 1
+            return
+
         new_node.next = self.head
         self.head = new_node
-        
-    def add_items_end(self, data):
+        self.size += 1
+
+    def addLast(self, data): #add last
         new_node = Node(data)
-        
         if self.head is None:
             self.head = new_node
-            return  
-            
-        current_node = self.head
-        while current_node.next is not None:
-            current_node = current_node.next
-            
-        current_node.next = new_node
-        
-    def remove_items_beginning(self):
-        if self.head is None:
-            print("List is empty.")
+            self.size += 1
             return
-        
+
+        curr_node = self.head
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+        curr_node.next = new_node
+        self.size += 1
+
+    def delete_first(self): #delete first
+        if self.head is None:
+            print("This list is empty.")
+            return
+        self.size -= 1
         self.head = self.head.next
-        
-    def remove_items_end(self):
+
+    def delete_last(self):
         if self.head is None:
-            print("List is empty.") 
+            print("This list is empty.")
             return
-        
+
+        self.size -= 1
         if self.head.next is None:
             self.head = None
             return
 
         second_last_node = self.head
         last_node = self.head.next
-        
         while last_node.next is not None:
-            second_last_node = last_node 
-            last_node = last_node.next  
-            
+            last_node = last_node.next
+            second_last_node = second_last_node.next
+
         second_last_node.next = None
-            
-    def print_link_list(self):
+
+
+    def get_size(self):
+        return self.size
+
+    def print_list(self):
         if self.head is None:
-            print("List is empty.") 
+            print("This list is empty.")
             return
-        
-        current_node = self.head
-        while current_node is not None:
-            print(current_node.data)
-            current_node = current_node.next  
-            
+
+        curr_node = self.head
+        while curr_node is not None:
+            print(curr_node.data, end= " ")
+            curr_node = curr_node.next
+
 if __name__ == "__main__":
-    obj = LL()
+    n = Linklist()
+    n.addLast(10)
+    n.addLast(20)
+    n.addLast(30)
+    n.delete_last()
+    n.delete_first()
+    n.print_list()
+    print("\nsize->", n.get_size())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
