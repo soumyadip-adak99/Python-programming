577bdcc0a8c45af8c72c235ef4897a564de13ae9
Soumya ! <soumya.dip.adak@outlook.com>
2025-12-15 23:08:38 +0530

    update

--- Django/storefront-01/storefront/settings.py
+++ Django/storefront-01/storefront/settings.py
@@ -36,9 +36,14 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "playground",
+    "debug_toolbar",
+    "store",
+    "tags",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -46,6 +51,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = "storefront.urls"
